22826a1b0b6019d332763cdde9dcc555e76258bb
Lordjayda <282820080+Lordjayda@users.noreply.github.com>
2026-05-18 20:44:40 +0200

    Delete multichatwindows-1.21.11 directory

--- multichatwindows-1.21.11/gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set DIR=%~dp0
-if defined JAVA_HOME (
-  set JAVA_EXE=%JAVA_HOME%in\java.exe
-) else (
-  set JAVA_EXE=java
-)
-"%JAVA_EXE%" -jar "%DIR%gradle\wrapper\gradle-wrapper.jar" %*
--- multichatwindows-1.21.11/src/main/java/com/client/multichatwindows/config/model/ServerConfig.java
+++
@@ -1,3 +0,0 @@
-package com.client.multichatwindows.config.model;
-import java.util.ArrayList; import java.util.List;
-public class ServerConfig { public int version=6; public String serverKey=""; public List<TabConfig> tabs=new ArrayList<>(); }
