8c2ac208535f7e05b6d00b84434a24a046dbc1d4
Lordjayda <282820080+Lordjayda@users.noreply.github.com>
2026-06-01 18:24:36 +0200

    Delete src/main directory

--- src/main/java/com/client/multichatwindows/MultiChatWindowsClient.java
+++
@@ -1,9 +0,0 @@
-package com.client.multichatwindows;
-import com.client.multichatwindows.config.ConfigManager;
-import com.client.multichatwindows.hud.HudOverlay;
-import com.client.multichatwindows.hud.WindowService;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-public class MultiChatWindowsClient implements ClientModInitializer {
-    @Override public void onInitializeClient(){ ConfigManager.init(); WindowService.rebuildForCurrentServer(); HudOverlay.init(); ClientPlayConnectionEvents.JOIN.register((handler,sender,client)->client.execute(WindowService::rebuildForCurrentServer)); }
-}
